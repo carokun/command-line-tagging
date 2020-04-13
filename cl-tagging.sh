6f6c36a2f81688620f09017389c71827534b702e
carokun <carokun@sas.upenn.edu>
2020-04-13 18:24:05 -0400

    initial bash commit

---
+++ cl-tagging.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#./cl-tagging.sh add <tagname> -f <filename> adds a tag to the specified file.
+#./cl-tagging.sh add <tagname> -d <folder>  adds a tag to all the files in the specified folder.
+#./cl-tagging.sh add <tagname> -d <folder> -e <extension>  adds a tag to all the files with the specified extension in the specified folder.
+#./cl-tagging.sh view <filename> prints all the tags (if any) for a given file.
+#./cl-tagging.sh list <tagname> prints a list of all the files with this tag.
+#./cl-tagging.sh copy <tagname> <new_directory> copies all the files with this tag to the given directory.
+
+has_f=1
+has_d=1
+has_e=1
+has_h=1
+command_type=$1
+file=""
+dir=""
+extension=""
+
+ctr=1
+for i in "$@"
+do
+    if [[ $i == "-h" ]]
+    then
+        cat help.txt
+        exit 0
+    elif [[ $i == "-f" ]]
+    then
+        has_f=0
+        j=$(($ctr+1))
+        file=${!j}
+    elif [[ $i == "-e" ]]
+    then
+        has_e=0
+        j=$(($ctr+1))
+        extension=${!j}
+    elif [[ $i == "-d" ]]
+    then
+        has_d=0
+        j=$(($ctr+1))
+        dir=${!j}
+    fi
+    ctr=$(($ctr+1))
+done
+
+if [[ $command_type == "add" ]]
+then
+    tag_name=$2
+    if [[ has_f -eq 0 ]]
+    then
+        echo python3 tag.py $command_type $tag_name $file
+    elif [[ has_e -eq 0 ]]
+    then
+        files=$(ls $dir | grep $extension | tr '\n' ' ')
+        echo python3 tag.py $command_type $tag_name $files
+    else
+        files=$(ls $dir | tr '\n' ' ')
+        echo python3 tag.py $command_type $tag_name $files
+    fi
+elif [[ $command_type == "view" ]]
+then
+    echo python3 tag.py $*
+elif [[ $command_type == "list" ]]
+then
+    echo python3 tag.py $*
+elif [[ $command_type == "copy" ]]
+then
+    tag_name=$2
+    dir=$3
+    files="$(python3 tag.py list $tag_name | tr '\n' ' ')"
+    cp $files $dir
+fi
+
