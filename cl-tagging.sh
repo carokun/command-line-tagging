9c64e54eec71d50928e711b10364d245c525e02b
carokun <carokun@sas.upenn.edu>
2020-04-13 18:27:34 -0400

    adding remove commands

--- cl-tagging.sh
+++ cl-tagging.sh
@@ -3,10 +3,13 @@
 #./cl-tagging.sh add <tagname> -f <filename> adds a tag to the specified file.
 #./cl-tagging.sh add <tagname> -d <folder>  adds a tag to all the files in the specified folder.
 #./cl-tagging.sh add <tagname> -d <folder> -e <extension>  adds a tag to all the files with the specified extension in the specified folder.
+#./cl-tagging.sh remove <tagname> -f <filename> removes a tag to the specified file.
+#./cl-tagging.sh remove <tagname> -d <folder>  removes a tag to all the files in the specified folder.
+#./cl-tagging.sh remove <tagname> -d <folder> -e <extension> removes a tag to all the files with the specified extension in the specified folder.
 #./cl-tagging.sh view <filename> prints all the tags (if any) for a given file.
 #./cl-tagging.sh list <tagname> prints a list of all the files with this tag.
 #./cl-tagging.sh copy <tagname> <new_directory> copies all the files with this tag to the given directory.
-
+ 
 has_f=1
 has_d=1
 has_e=1
@@ -47,21 +50,35 @@ then
     tag_name=$2
     if [[ has_f -eq 0 ]]
     then
-        echo python3 tag.py $command_type $tag_name $file
+        python3 tag.py $command_type $tag_name $file
     elif [[ has_e -eq 0 ]]
     then
         files=$(ls $dir | grep $extension | tr '\n' ' ')
-        echo python3 tag.py $command_type $tag_name $files
+        python3 tag.py $command_type $tag_name $files
     else
         files=$(ls $dir | tr '\n' ' ')
-        echo python3 tag.py $command_type $tag_name $files
+        python3 tag.py $command_type $tag_name $files
+    fi
+elif [[ $command_type == "remove" ]]
+then
+    tag_name=$2
+    if [[ has_f -eq 0 ]]
+    then
+        python3 tag.py $command_type $tag_name $file
+    elif [[ has_e -eq 0 ]]
+    then
+        files=$(ls $dir | grep $extension | tr '\n' ' ')
+        python3 tag.py $command_type $tag_name $files
+    else
+        files=$(ls $dir | tr '\n' ' ')
+        python3 tag.py $command_type $tag_name $files
     fi
 elif [[ $command_type == "view" ]]
 then
-    echo python3 tag.py $*
+    python3 tag.py $*
 elif [[ $command_type == "list" ]]
 then
-    echo python3 tag.py $*
+    python3 tag.py $*
 elif [[ $command_type == "copy" ]]
 then
     tag_name=$2
