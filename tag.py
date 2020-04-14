4b756a08dc582ccb4385873a8d4be3ec9335b2a8
Priya Gupta <priyagupta@priyas-mbp.lan>
2020-04-14 13:40:53 -0400

    remove command

--- tag.py
+++ tag.py
@@ -1,7 +1,6 @@
 import sys
 import pickle
 
-
 """
 Adds the specified tag to the specified files by updating 
 the tag_to_files and file_to_tags dictionaries
@@ -19,6 +18,26 @@ def add_tags(tag, files, tag_to_files, file_to_tags):
 		else:
 			file_to_tags[file] = set()
 			file_to_tags[file].add(tag)
+
+	save_data(tag_to_files, file_to_tags)
+
+"""
+Removes the speciifed tag from the specified files by 
+updating the tag_to_files and file_to_tags dictionaries
+"""
+def remove_tags(tag, files, tag_to_files, file_to_tags):
+	if tag in tag_to_files:
+		tag_to_files[tag].difference_update(files)
+
+		if len(tag_to_files[tag]) == 0:
+			del tag_to_files[tag]
+
+	for file in files:
+		if file in file_to_tags:
+			file_to_tags[file].remove(tag)
+
+			if len(file_to_tags[file]) == 0:
+				del file_to_tags[file]
 
 	save_data(tag_to_files, file_to_tags)
 
@@ -61,27 +80,37 @@ def save_data(tag_to_files, file_to_tags):
 	pickle.dump(file_to_tags, open(".file_to_tags.p", "wb"))
 
 
-def main(argv):
+def main(argv):	
+	if len(argv) == 0:
+		print("No command inputted!")
+
 	tag_to_files, file_to_tags = load_data()
 	command = argv[0].lower()
 
-	if command == 'add':
+	if command == "add":
 		tag = argv[1].lower()
 		files = argv[2:]
 		add_tags(tag, files, tag_to_files, file_to_tags)
 
-	if command == "view":
+	elif command == "remove":
+		tag = argv[1].lower()
+		files = argv[2:]
+		remove_tags(tag, files, tag_to_files, file_to_tags)
+
+	elif command == "view":
 		file = argv[1]
 		tags = view_tags(file, file_to_tags)
 		tags_str = ', '.join([str(elem) for elem in tags])
 		print("The tags for", file, "are:", tags_str)
 
-
-	if command == "list":
+	elif command == "list":
 		tag = argv[1].lower()
 		files = view_files(tag, tag_to_files)
 		files_str = ', '.join([str(elem) for elem in files])
 		print("The files tagged with", tag, "are:", files_str)
-	 
+
+	else: 
+		print("Command not valid!")
+
 if __name__ == "__main__":
    main(sys.argv[1:])
